7a6fe9bd9c2fe29da8d648d26bc8b661b19264f9
Tony Mountifield <tony@mountifield.org>
2022-11-05 20:43:23 +0000

    Add some extra CORS origin domains for testing.

--- servers.php
+++ servers.php
@@ -6,7 +6,10 @@ $origins = array(
 	'http://jamulus.softins.co.uk:8080' => 1,
 	'http://explorer.jamulus.io' => 1,
 	'https://explorer.jamulus.io' => 1,
-	'http://explorer.jamulus.io:8080' => 1
+	'http://explorer.jamulus.io:8080' => 1,
+	'http://explorer.softins.co.uk' => 1,
+	'https://explorer.softins.co.uk' => 1,
+	'http://explorer.softins.co.uk:8080' => 1
 );
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
