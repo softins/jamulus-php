8c5020ac51ec26d86ce1b1b3cd3e3de995876235
Tony Mountifield <tony@mountifield.org>
2022-11-14 12:26:18 +0000

    Add shim for London AWS

---
+++ servers-lon2.php
@@ -0,0 +1,28 @@
+<?php
+
+$origins = array(
+	'http://jamulus.softins.co.uk' => 1,
+	'https://jamulus.softins.co.uk' => 1,
+	'http://jamulus.softins.co.uk:8080' => 1,
+	'http://explorer.jamulus.io' => 1,
+	'https://explorer.jamulus.io' => 1,
+	'http://explorer.jamulus.io:8080' => 1,
+	'http://explorer.softins.co.uk' => 1,
+	'https://explorer.softins.co.uk' => 1,
+	'http://explorer.softins.co.uk:8080' => 1
+);
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+	$http_origin = $_SERVER['HTTP_ORIGIN'];
+
+	if (array_key_exists($http_origin, $origins))
+	{
+		header("Access-Control-Allow-Origin: $http_origin");
+		header("Vary: Origin");
+	}
+}
+
+header('Content-Type: application/json');
+
+readfile("http://18.133.158.130/jamulus-php-master/servers.php?" . $_SERVER['QUERY_STRING']);
+?>
